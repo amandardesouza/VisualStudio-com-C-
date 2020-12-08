21962b200b84c39f4b03f5b9b2c2e552a42fec3c
amandardesouza <amanda.r.de.souza@avanade.com>
2020-12-08 19:49:01 -0300

    Adicionar arquivos de projeto.

---
+++ TakeTestExemplo/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Drawing;
+
+
+
+namespace TakeTestExemplo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Window.Maximize();
+            //driver.Quit();
+
+
+        }
+    }
+}
